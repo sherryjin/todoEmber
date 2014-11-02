242ac83c6e14cad664342810f0835d3545c89d9f
Sherry Jin <boatbound.co>
2014-11-01 20:55:17 -0700

    done

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -12,7 +12,13 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       this.set('newTitle', '');
       todo.save();
-    }
+    },
+
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
+    },
   },
   
   remaining: function(){
@@ -22,6 +28,23 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function(){
     var remaining = this.get('remaining');
     return remaining === 1 ? 'item': 'items';
-  }.property('remaining')
+  }.property('remaining'),
 
+  hasCompleted: function(){
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function() {
+    return this.filterBy("isCompleted", true).get('length');
+  }.property('@each.isCompleted'),
+
+  allAreDone: function(key, value) {
+    if (value === undefined){
+      return !!this.get('length') && this.isEvery('isCompleted');
+    } else {
+      this.setEach('isCompleted', value);
+      this.invoke('save');
+      return value;
+    }
+  }.property('@each.isCompleted'),
 });
